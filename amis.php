303014964122f087ba6581898d1b4a6a00480a7e
Eddy MONTUS <eddy.montus@gmail.com>
2013-09-26 13:05:30 +0000

    ajout d'un lien vers legitemarengo

--- amis.php
+++ amis.php
@@ -11,7 +11,7 @@
 			<div class="content">
 				<center>
 					<a href="http://www.emreparation.fr">Un des meilleur service de réparation pc et téléphone</a><br/>
-					<a href="http://lamaisondesvignes.free.fr">Une super chambre d'hôtes</a><br/>
+					<a href="http://lamaisondesvignes.free.fr">Une super chambre d'hôtes</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a href="http://www.eddymontus.fr/clients/legitemarengo/">gite</a><br/>
 					<a href="http://www.air3bordeaux.org">Mon ancienne équipe de course à pied</a>
 				</center>
 			</div>
