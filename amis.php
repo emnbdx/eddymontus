52f5a2a9883c9024c25a9fb3d6a1fe8c86048d07
Eddy MONTUS <eddy.montus@gmail.com>
2014-05-19 14:27:26 +0000

    pointage vers le vrai site du gite marengo

--- amis.php
+++ amis.php
@@ -11,7 +11,7 @@
 			<div class="content">
 				<center>
 					<a href="http://www.emreparation.fr">Un des meilleurs services de réparation PC et téléphone</a><br/>
-					<a href="http://lamaisondesvignes.free.fr">Une super chambre d'hôtes</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a href="http://www.eddymontus.fr/clients/legitemarengo/">gite</a><br/>
+					<a href="http://lamaisondesvignes.free.fr">Une super chambre d'hôtes</a>&nbsp;&nbsp;/&nbsp;&nbsp;<a href="http://www.legitemarengo.com">gite</a><br/>
 					<a href="http://www.air3bordeaux.org">Mon ancienne équipe de course à pied</a>
 				</center>
 			</div>
