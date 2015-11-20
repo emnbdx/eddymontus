761e775b3d658ab58e6ca3f0f7ed24183c1ef7be
Eddy MONTUS <eddy.montus@gmail.com>
2015-11-20 17:25:17 +0100

    remise en place du lien lamaisondesvignes.

--- amis.php
+++ amis.php
@@ -11,6 +11,7 @@
 			<div class="content friends">
 				<center>
 					<a href="http://www.emreparation.fr" target="_blank" title="Un des meilleurs services de réparation PC et téléphone"><img src="img/emreparation.jpg" /></a>
+					<a href="http://www.lamaisondesvignes.fr" target="_blank" title="Une super chambre d'hôtes"><img src="img/lamaisondesvignes.jpg" /></a>
 					<a href="http://www.legitemarengo.com" target="_blank" title="Un super gite"><img src="img/legitemarengo.jpg" /></a>
 					<a href="http://www.jetalize.fr" target="_blank" title="De la location de Jet Ski à La Tremblade"><img src="img/jetalize.jpg" /></a>
 					<a href="http://www.air3bordeaux.org" target="_blank" title="Mon ancienne équipe de course à pied"><img src="img/air3bordeaux.jpg" /></a>
