5ec355323f200dc949093ea92110ec9217a4a9ef
Eddy <eddy.montus@gmail.com>
2018-04-06 12:00:45 +0200

    Update amis.php

--- amis.php
+++ amis.php
@@ -10,7 +10,7 @@
 		<div class="container">
 			<div class="content friends">
 				<center>
-					<a href="http://www.emreparation.fr" target="_blank" title="Un des meilleurs services de réparation PC et téléphone"><img src="img/emreparation.jpg" /></a>
+					<a href="/domains/emreparation" target="_blank" title="Un des meilleurs services de réparation PC et téléphone"><img src="img/emreparation.jpg" /></a>
 					<a href="http://www.lamaisondesvignes.fr" target="_blank" title="Une super chambre d'hôtes"><img src="img/lamaisondesvignes.jpg" /></a>
 					<a href="http://www.legitemarengo.com" target="_blank" title="Un super gite"><img src="img/legitemarengo.jpg" /></a>
 					<a href="http://www.jetalize.fr" target="_blank" title="De la location de Jet Ski à La Tremblade"><img src="img/jetalize.jpg" /></a>
